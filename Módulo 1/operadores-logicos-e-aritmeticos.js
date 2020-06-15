36f2980b5845a11b986805df357f5c14a791bd59
Christopher Costa <contato.christopher@gmail.com>
2020-06-15 14:27:15 -0300

    Add descriptions

---
+++ Módulo 1/operadores-logicos-e-aritmeticos.js
@@ -0,0 +1,18 @@
+/*
+
+Lógicos:
+&&  Conforme a tabela verdade AND
+||  Conforme a tabela verdade OR
+!   Nega o booleano, not
+
+O que converte para falso: null, NaN, 0, "", undefined
+-------------------------------
+
+Aritméticos:
++   Soma
+-   Subtração
+/   Divisão
+*   Multiplicação
+%   Resto da divisão
+
+/*
---
+++ Módulo 1/operadores-relacionais-e-comparativos.js
@@ -0,0 +1,12 @@
+/*
+
+>       Maior
+>=      Maior ou igual
+<       Menor
+<=      Menor ou Igual
+==      Igual
+===     Igual e do mesmo tipo
+!=      Diferente de
+!===    Diferente, inclsive do mesmo tipo
+
+*/
