1c15d8e63534b6dc7cc42260c758fb51b141ae53
Christopher Costa <contato.christopher@gmail.com>
2020-06-17 00:22:20 -0300

    add boolean.ts

---
+++ modulo_01/9.booleano.js
@@ -0,0 +1,52 @@
+/* 
+Esses valores são falsy, considerados como false
+if (false)
+if (null)
+if (undefined)
+if (0)
+if (NaN)
+if ('')
+
+truty são todos diferente de falsy, alguns exemplos:
+if (true), if ({})
+if ([]), if (42), if ("foo")
+if (new Date()) , if (-42) , if (3.14)
+if (-3.14), if (Infinity), if (-Infinity)
+*/
+
+const alunos = [
+    {
+        nome: 'Fulano',
+        nota: 5,
+    },
+    {
+        nome: 'Beltrano',
+        nota: 9.5,
+    },
+    {
+        nome: 'Ciclano',
+        nota: 10,
+    }
+];
+
+const marcaComoReprovado = aluno => {
+    aluno.reprovado = false
+    if (aluno.nota <= 5) aluno.reprovado = true;
+}
+
+const enviaMensagemReprovado = aluno => {
+    if(aluno.reprovado) {
+        console.log(`O aluno ${aluno.nome} está reprovado!`);
+    }
+}
+
+const alunoReprovado = alunos => {
+    for (aluno of alunos) {
+        marcaComoReprovado(aluno);
+        enviaMensagemReprovado(aluno);
+    }
+}
+
+alunoReprovado(alunos);
+console.table(alunos);
+
